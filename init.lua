da55e28cac69268f22717a763ae76f63b95f2c17
Parin810 <parinchheda810@gmail.com>
2018-06-16 22:09:42 +0530

    sorted time issues

--- init.lua
+++ init.lua
@@ -1,26 +1,81 @@
-ssid = "ERTS LAB 304"
-password = "balstre403"
+
+ssid='ERTS LAB 304'
+password='balstre403'
 
 
-wifi.setmode(wifi.STATION)
-wifi.sta.config(ssid,password)
-
-wifi.sta.disconnect()
---bat3=tostring(adc.readvdd33())
-wifi.sta.connect()
-
---configure gpio pins according to revised pin map
 pin14 = 5
 pin12 = 6
+pin4 = 1
+pin5 = 2
+pin13 = 7
+
+gpio.mode(pin14,gpio.OUTPUT) --in1 as output
+gpio.mode(pin12,gpio.OUTPUT) --in2 as output
+gpio.mode(pin4,gpio.OUTPUT) --control battery charging
+gpio.write(pin4,gpio.LOW)
 
 
-gpio.mode(pin14,gpio.OUTPUT)
-gpio.mode(pin12,gpio.OUTPUT)
-gpio.write(pin14,gpio.LOW)
-gpio.write(pin12,gpio.LOW)
+gpio.mode(pin5,gpio.OUTPUT) 
+gpio.write(pin5,gpio.LOW)
+
+gpio.write(pin12,gpio.LOW) --in1 for driver (sleep mode)
+gpio.write(pin14,gpio.LOW) --in2 for driver (sleep mode)
 
 
-c=false                                   --initialising the flag 
-wifiAvailable=0
-sleepTime=1000000*60*1    --1 min sleep
-dofile('connect.lua')
+
+
+node.setcpufreq(node.CPU80MHZ) --reduce operating freq for power saving
+
+wifi.setmode(wifi.STATION)
+
+--wifi.sta.config(ssid,password)
+
+
+--rtcmem.write32(0,0,1)
+--val1, val2 = rtcmem(0,2)
+--print(val1)
+--print (val2)
+
+x = "5435"
+--
+val = rtcmem.read32(5)
+--val = string.char(val)
+if val==101 then
+	print ("stop")
+	gpio.write(pin14,gpio.HIGH)
+    gpio.write(pin12,gpio.LOW)
+	tmr.alarm(2,80,0,function()
+	     gpio.write(pin12,gpio.LOW)
+	     gpio.write(pin14,gpio.LOW)
+     end)
+end
+print(val)
+rtcmem.write32(5,0)
+
+station_cfg={}
+station_cfg.ssid="ERTS LAB 304"
+station_cfg.pwd="balstre403"
+--station_cfg.save=false
+wifi.sta.config(station_cfg)
+
+-- cfg={
+  -- ip = "192.168.1.123",
+  -- netmask = "255.255.0.0",
+  -- gateway = "192.168.1.1"
+-- }
+
+-- --wifi.sta.sleeptype(wifi.LIGHT_SLEEP)
+
+wifi.sta.connect()
+
+
+
+
+--configure gpio pins according to revised pin map
+
+                                  
+
+wifiAvailable=0 --initialising the flag 
+sleepTime=1000000*60*10   --10 min sleep
+
+dofile('connect1.lua')
--- mqttsetup_valve.lua
+++ mqttsetup_valve.lua
@@ -1,5 +1,5 @@
 --creating client with the macid as the client id 
-m = mqtt.Client(wifi.sta.getmac(), 10)   --10sec keep alive
+m = mqtt.Client(wifi.sta.getmac(),60)   --10sec keep alive
 --lwt function sends the last will and testament message to tbe sent to the broker in case it goes offline
 --m:lwt('lwt','offline',0,0)    
 
@@ -8,6 +8,9 @@ m:on('offline',function(con) c = false end)
 -- on publish message receive event print the topic and data and do the task
 m:on('message', function(conn, topic, data)
   print(topic .. ':')
+  if topic == 'esp/'..macid..'/irrigate' then
+	irrigate(data)
+  end
   if data ~= nil then
     print(data)
   end 
@@ -21,33 +24,50 @@ m:on('message', function(conn, topic, data)
   elseif data == "2" then   
     -- paylaod format identifier,bat3,bat6
     payload='1,'..tostring(bat3)..','..tostring(adc.read(0))
-    m:publish('esp/'..macid..'/battery',payload,0,0, function(conn) end)--print('battery status sent') end)
+    m:publish('esp/'..macid..'/battery',payload,0,0, function(conn) end)
     print("Battery status sent ")  
-  elseif tonumber(data)>10  then 
-    sleepTime = tonumber(data)*1000000*60
-    print("Sleep time set to ".. tostring(sleepTime))    
+  elseif data =="3" then 
+	print ("sleep")
     node.dsleep(sleepTime)
-  elseif data=="3" then 
-    node.dsleep(sleepTime)
-    
    else
   end
 end)
 
 function startValve()
+	gpio.mode(pin13,gpio.OUTPUT)
+	gpio.write(pin13,gpio.LOW)
+	tmr.delay(1000)
     gpio.write(pin12,gpio.HIGH)
     gpio.write(pin14,gpio.LOW)
-    doValve()
-end
-function stopValve()
 
-    gpio.write(pin12,gpio.LOW)
-    gpio.write(pin14,gpio.HIGH)
+    tmr.alarm(2,80,0,function()
+	     gpio.write(pin12,gpio.LOW)
+	     gpio.write(pin14,gpio.LOW)
+    end)
     
-    doValve()
 end
-function doValve()
-  gpio.write(pin13,gpio.HIGH) 
-  tmr.delay(80000)
-  gpio.write(pin13,gpio.LOW)
+
+
+function irrigate(r_time)
+	Time = 1000000*30*r_time
+	rtcmem.write32(5,string.byte('e'))
+	startValve()
+	wifi.sta.disconnect()
+	print("sleep")
+	node.dsleep(Time)
 end
+
+function stopValve()
+	gpio.mode(pin13,gpio.OUTPUT)
+	gpio.write(pin13,gpio.LOW)
+	tmr.delay(1000)
+    gpio.write(pin14,gpio.HIGH)
+    gpio.write(pin12,gpio.LOW)
+
+     tmr.alarm(2,80,0,function()
+	     gpio.write(pin12,gpio.LOW)
+	     gpio.write(pin14,gpio.LOW)
+     end)
+
+end
+
